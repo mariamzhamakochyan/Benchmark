c7a29d612ca2e7133b41cfc04564e45de2e8df81
Mariam Zhamakochyan <mariamzhamakochyan@gmail.com>
2023-05-16 19:44:43 +0400

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -1,33 +1,46 @@
 import time
 import random
+import numpy as np
 
-def delete(vector, element):
+def delete_vector(vector, element):
     start_time = time.time()
-    if element in vector:
-        vector.remove(element)
+    vector = np.delete(vector, np.where(vector == element))
     end_time = time.time()
     return end_time - start_time
 
-def insert(col, index, element):
+def insert_vector(vector, index, element):
+    start_time = time.time()
+    vector = np.insert(vector, index, element)
+    end_time = time.time()
+    return end_time - start_time
+    
+def delete_list(col, element):
+    start_time = time.time()
+    if element in vector:
+        col.remove(element)
+    end_time = time.time()
+    return end_time - start_time
+
+def insert_list(col, index, element):
     start_time = time.time()
     col.insert(index, element)
     end_time = time.time()
     return end_time - start_time
     
 n = int(input("How many elements do you want to generate? "))
-random_elements = [random.randint(1,100) for _ in range(n)]
+random_elements1 = np.random.randint(1, 100, size=n)
+random_elements2 = [random.randint(1,100) for _ in range(n)]
 
-vector = random_elements.copy()
-lst = random_elements.copy()
+vector = random_elements1.copy()
+lst = random_elements2.copy()
 
-delete_time_vector = delete(vector, 3)
-insert_time_vector = insert(vector, 2, 10)
+delete_time_vector = delete_vector(vector, 3)
+insert_time_vector = insert_vector(vector, 2, 10)
 
-delete_time_list = delete(lst, 3)
-insert_time_list = insert(lst, 2, 10)
+delete_time_list = delete_list(lst, 3)
+insert_time_list = insert_list(lst, 2, 10)
 
 print("Delation from vector: {:.6f} seconds".format(delete_time_vector))
 print("Insert in vector: {:.6f} seconds".format(insert_time_vector))
 print("Delation from list: {:.6f} seconds".format(delete_time_list))
 print("Insert in list: {:.6f} seconds".format(insert_time_list))
-
